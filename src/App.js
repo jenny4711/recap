5ba2004954968c3c74b38f1b11bfba5eff43fb35
jiylee <jjenny4711@gmail.com>
2023-12-19 14:40:25 -0500

    ff

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React, { useState } from 'react';
 
-import { getAuth, RecaptchaVerifier ,initializeAuth,} from "firebase/auth";
+import { getAuth, RecaptchaVerifier} from "firebase/auth";
 function App() {
   const [captchaValue, setCaptchaValue] = useState(null);
-  const firebaseConfig = {
  
-    apiKey: "AIzaSyBmSs3e-RrLAgTp_KmBTViNO2iHGQgClJA",
-    authDomain: "appchat-17fa8.firebaseapp.com",
-    projectId: "appchat-17fa8",
-    storageBucket: "appchat-17fa8.appspot.com",
-    messagingSenderId: "595341060110",
-    appId: "1:595341060110:web:5f9871d5e4e06b1ccb345f"
-  };
-  if(!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig)
-  }
-  const app = firebase.app();
-  const auth = getAuth(app)
+  const auth = getAuth()
   function cap(){
     window.recaptchaVerifier = new RecaptchaVerifier(auth, 'sign-in-button', {
       'size': 'invisible',
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmSs3e-RrLAgTp_KmBTViNO2iHGQgClJA",
+  authDomain: "appchat-17fa8.firebaseapp.com",
+  projectId: "appchat-17fa8",
+  storageBucket: "appchat-17fa8.appspot.com",
+  messagingSenderId: "595341060110",
+  appId: "1:595341060110:web:5f9871d5e4e06b1ccb345f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
